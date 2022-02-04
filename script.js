488b14f756229f6a863f9ff50f50102f1e840591
boms0q <98953762+boms0q@users.noreply.github.com>
2022-02-04 18:37:28 +0100

    save

--- script.js
+++
@@ -1,8 +0,0 @@
-const PlayButton = document.getElementsByClassName('HomePlayButton')[0]
-
-toggleButton.addEventListener('click', () => {
-    window.scrollTo({
-        top: 50,
-        behavior: "smooth"
-    })
-})
